50465610305d1843abb40463aacc90d2391914c3
Anahid <anahidhemmat2130@gmail.com>
2022-03-03 18:17:38 +0400

    form input event

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,3 +5,15 @@ const image = document.querySelector(".day-night");
 const cityName = document.querySelector(".city-name");
 const weatherCondition = document.querySelector(".weather-condition");
 const temprature = document.querySelector(".temp");
+
+// form input event
+form.addEventListener("submit", (e) => {
+  //prevent default behaivior
+  e.preventDefault();
+  //get input value
+  const city = form.city.value.trim();
+  //reset formx
+  form.reset();
+  //update city
+  updateCity(city);
+});
