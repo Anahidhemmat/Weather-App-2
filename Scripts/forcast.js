cc0f5a2220c42f8f07d04548081eb4e5c0f4dbf3
Anahid <anahidhemmat2130@gmail.com>
2022-03-03 16:31:17 +0400

    get city information with API

---
+++ Scripts/forcast.js
@@ -0,0 +1,16 @@
+const key = "R0kHJGpfEsVALM8nGPwGxeN0no5fQsPA";
+
+// get city information with API
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("Tbilisi").then((data) => {
+  console.log(data);
+});
