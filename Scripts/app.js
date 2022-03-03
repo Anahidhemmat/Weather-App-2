a3c8203b35a5714e7550bb77921db093a340abb6
Anahid <anahidhemmat2130@gmail.com>
2022-03-03 18:07:24 +0400

    selectors

---
+++ Scripts/app.js
@@ -0,0 +1,7 @@
+//selectors
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
+const image = document.querySelector(".day-night");
+const cityName = document.querySelector(".city-name");
+const weatherCondition = document.querySelector(".weather-condition");
+const temprature = document.querySelector(".temp");
